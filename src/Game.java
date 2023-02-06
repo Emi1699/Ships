8d218a3859a1693d843cc50d29f542d50440c380
Emanuel Buliga <emanuel.buliga@phipartners.com>
2023-02-06 11:59:17 +0200

    testing gitignore

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,7 @@ public class Game {
     public void start() throws InterruptedException {
         Thread.sleep(2000);
         System.out.println("Game has started!\n\n");
-
+        System.out.println("Game has started!\n\n");
 
 
     }
