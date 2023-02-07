c4c0df096a7d4a80bfde3318f67340ab0d72ef26
Emanuel Buliga <emanuel.buliga@phipartners.com>
2023-02-07 16:16:53 +0200

    Can sucessfully generate map, although there is a small problem: when trying to print out the Id's of the map's zones, all their values are null - have to fix this

--- src/gameEntities/Map.java
+++ src/gameEntities/Map.java
@@ -1,2 +1,53 @@
-package gameEntities;public class Map {
+package gameEntities;
+
+import exceptions.InvalidZoneIdException;
+import game.Updateable;
+
+import java.util.ArrayList;
+
+public class Map implements Updateable {
+    private static int WIDTH; // number of horizontal cells (bytes)
+    private static int HEIGHT; // number of vertical cells  (chars)
+
+    // used to access the map's zones by their names
+    private final ArrayList<Zone> zones = new ArrayList<>();
+
+    // create a new map
+    public Map() throws InvalidZoneIdException {
+        for (int row = 'A'; row < 'A' + Map.HEIGHT; row++) {
+            for (int column = 1; column <= Map.WIDTH; column++) {
+                zones.add(new Zone(row + Integer.toString(column)));
+            }
+        }
+    }
+
+    public Map(int width, int height) throws InvalidZoneIdException {
+        Map.WIDTH = width;
+        Map.HEIGHT = height;
+
+        for (char row = 'A'; row < 'A' + Map.HEIGHT; row++) {
+            for (int column = 1; column <= Map.WIDTH; column++) {
+                zones.add(new Zone(row + Integer.toString(column)));
+            }
+        }
+    }
+
+    public void getZones() {
+        int i = 1;
+        for (Zone zone : zones) {
+            System.out.print(zone.getId() + " ");
+            i++;
+
+            // print a new line
+            if (i == Map.WIDTH) {
+                System.out.println();
+                i = 1;
+            }
+        }
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
